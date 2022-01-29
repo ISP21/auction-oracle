bbe59a3a45688dbede679030063505d6dd451b10
jbrucker <jebrucker@gmail.com>
2022-01-29 09:11:56 +0700

    Modify the runtests.sh script to print a summary of all the test runs and return an exit code equal to the number of failing scenarios. This is so the Github Action will accurately show if the overall job succeeded or failed.

--- runtests.sh
+++ runtests.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-TESTMODULE="test_auction.py"
+if [ -f "test_auction.py" ]; then
+	TESTMODULE="test_auction.py"
+elif [ -f "auction_test.py" ]; then
+	TESTMODULE="auction_test.py"
+else
+	echo "No test file test_auction.py or auction_test.py"
+	exit 9
+fi
+
+# arrays to record results. Elements are appended in runtests()
+expect=("")
+actual=("")
 
 drawline( ) {
     echo "----------------------------------------------------------------------"
 }
+
 runtests( ) {
 	for testcase in 1 2 3 4 5 6 7 8; do
         echo ""
         drawline
+        # append new element to expect. All testcases should fail except 1.
+        expect+=("FAIL")
 		case $testcase in
     	1)
 			echo "AUCTION CODE 1: All methods work according to specification. Your tests should PASS."
+			# this case should pass all tests
+			expect[1]="OK"
        		;;
 		3)
 			echo "AUCTION CODE 3: The auction is not rejecting some invalid bids."
@@ -32,9 +48,35 @@ runtests( ) {
         drawline
 		export TESTCASE=$testcase
 		python3 -m unittest -v $TESTMODULE
+		# record status
+		if [ $? -eq 0 ]; then
+			actual+=("OK")
+		else
+			actual+=("FAIL")
+		fi
 		# wait til user presses enter
 		# read input
 	done
 }
 
+showresults() {
+    drawline
+    echo "RESULTS of 8 Auction Tests"
+    drawline
+    echo "OK=all tests pass, FAIL=some tests fail"
+    echo ""
+    echo "Auction#  Expect  Actual"
+    failures=0
+	for testcase in 1 2 3 4 5 6 7 8; do
+        printf "%5d      %-4s     %s\n" ${testcase} ${expect[$testcase]} ${actual[$testcase]}
+        if [ ${expect[$testcase]} != ${actual[$testcase]} ]; then
+			failures=$(($failures+1))
+		fi
+	done
+    correct=$((8-$failures))
+	echo "$correct Correct  $failures Incorrect"
+    exit $failures
+}
+
 runtests
+showresults
